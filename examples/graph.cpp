8e8e8a91266e2f4cd64c1b14e8c48e58351358e2
Marcos Mayorga <mm@mm-studios.com>
2016-10-03 18:24:01 +0100

    graph

---
+++ examples/graph.cpp
@@ -0,0 +1,83 @@
+#include <mstd/graph>
+using namespace mstd;
+
+int main() {
+	vector<string> al;
+	al.push_back("1 2");
+	al.push_back("1 3");
+	al.push_back("1 4");
+	al.push_back("2 5");
+	al.push_back("2 6");
+	al.push_back("3 7");
+	al.push_back("4 8");
+	al.push_back("4 9");
+	al.push_back("5 10");
+	al.push_back("5 11");
+	al.push_back("6 12");
+	al.push_back("7 13");
+	al.push_back("8 14");
+	al.push_back("14 8");
+	al.push_back("9 2");
+	al.push_back("6 2");
+	al.push_back("12 3");
+	
+	graph g(al);
+	g.dot(cout);
+
+	struct vis:graph::visitor {
+		void start(const vertex&) override {};
+		void visit(const edge& e) override {
+			cout << (e.from!=0?e.from->id:-1) << " -> " << e.to->id << endl;
+		}
+		void finished() override {};
+	};
+
+	vis vis_;
+	cout << "breath first traversal" << endl;
+	g.breath_first(1,vis_);
+	cout << "depth first traversal" << endl;
+	g.depth_first(1,vis_);
+
+	cout << "best path" << endl;
+	vector<string> distances;
+	distances.push_back("1 2 1");
+	distances.push_back("1 3 1");
+	distances.push_back("1 4 1");
+	distances.push_back("2 5 1");
+	distances.push_back("2 6 1");
+	distances.push_back("3 7 1");
+	distances.push_back("4 8 1");
+	distances.push_back("4 9 1");
+	distances.push_back("5 10 1");
+	distances.push_back("5 11 1");
+	distances.push_back("6 12 1");
+	distances.push_back("7 13 1");
+	distances.push_back("8 14 1");
+	distances.push_back("14 8 1");
+	distances.push_back("9 2 1");
+	distances.push_back("6 2 1");
+	distances.push_back("12 3 1");
+
+	typedef best_path<scalar<int>,data> pathfinder;
+	pathfinder bp(g,distances);
+	{
+	cout << "best path from 1 to 11 )" << endl;
+	auto r=bp.compute(1,11,pathfinder::breath_first);
+	r.dump(cout);
+	}
+	{
+	cout << "best path from 14 to 2 )" << endl;
+	auto r=bp.compute(14,2,pathfinder::breath_first);
+	r.dump(cout);
+	}
+	{
+	cout << "best path from 1 to 2 )" << endl;
+	auto r=bp.compute(1,2,pathfinder::depth_first);
+	r.dump(cout);
+	}
+
+
+	return 0;
+}
+
+
