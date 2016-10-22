9d9832b48dbac577b715915a9d1f3177a4813fcb
marcos <marcos@jamon.mm-studios.com>
2016-10-22 18:07:44 +0100

    added kurskal algorithm to graph for minimum spanning tree

--- examples/graph.cpp
+++ examples/graph.cpp
@@ -1,7 +1,56 @@
 #include <mstd/graph>
+#include <fstream>
 using namespace mstd;
 
+void kruskal_minimum_spanning_tree_demo() {
+
+	vector<string> al={
+	"0 1 4",
+	"0 7 8",
+	"1 7 11",
+	"1 2 8",
+	"7 8 7",
+	"2 8 2",
+	"7 6 1",
+	"6 8 6",
+	"2 5 4",
+	"6 5 2",
+	"2 3 7",
+	"3 5 14",
+	"3 4 9",
+	"5 4 10"};
+
+	graph<> g(al,graph<>::undirected);
+
+	{
+	cout << "written dot file: graph.dot" << endl;
+	ofstream f("graph.dot");
+	g.dot(f);
+	}
+
+	cout << g.vertexes_size() << " vertexes, " << g.edges_size() << " edges " << endl;
+
+	auto v=g.sort_edges_no_descendent();
+	cout << "Weight Src Dst" << endl;
+	for (auto& i:v) {
+		cout << g.get_edata(*i).value << " " << i->from->id << " " << i->to->id << endl;
+	}
+
+	auto mstk=g.minimum_spanning_tree_kruskal();
+
+	{
+	cout << "written dot file: graph_mst_kruskal.dot" << endl;
+	ofstream f("graph_mst_kruskal.dot");
+	mstk.dot(f);
+	}
+
+}
+
+
 int main() {
+	kruskal_minimum_spanning_tree_demo();
+	return 0;
+
 	vector<string> al;
 	al.push_back("1 2");
 	al.push_back("1 3");
@@ -21,13 +70,18 @@ int main() {
 	al.push_back("6 2");
 	al.push_back("12 3");
 	
-	graph g(al);
-	g.dot(cout);
+	graph<> g(al);
 
-	struct vis:graph::visitor {
+	{
+	cout << "written dot file: graph.dot" << endl;
+	ofstream f("graph.dot");
+	g.dot(f);
+	}
+
+	struct vis:basic_graph::visitor {
 		void start(const vertex&) override {};
 		void visit(const edge& e) override {
-			cout << (e.from!=0?e.from->id:-1) << " -> " << e.to->id << endl;
+			cout << "   " << (e.from!=0?e.from->id:-1) << " -> " << e.to->id << endl;
 		}
 		void finished() override {};
 	};
@@ -58,7 +112,7 @@ int main() {
 	distances.push_back("6 2 1");
 	distances.push_back("12 3 1");
 
-	typedef best_path<scalar<int>,data> pathfinder;
+	typedef best_path<scalar<int>> pathfinder;
 	pathfinder bp(g,distances);
 	{
 	cout << "best path from 1 to 11 )" << endl;
